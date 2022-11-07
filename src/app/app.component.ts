477b602765f0c5a4fcf3cd22b7745d57198dd49f
Lovel Piližota <lovel.pilizota@gmail.com>
2022-11-07 03:08:30 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { AddComponent } from './add/add.component';
 import { DetailComponent } from './detail/detail.component';
@@ -15,24 +16,25 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit{
   title = 'employeeManagerFrontend';
-  displayedColumns = ['first_name', 'last_name', 'department', 'action'];
+  displayedColumns = ['firstName', 'lastName', 'department', 'action'];
 
   public employees?: Employee[];
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
-
+  @ViewChild(MatSort) sort!: MatSort;
 
 
   constructor(private employeeService: EmployeeService, private dialog: MatDialog) {
-    this.dataSource = new MatTableDataSource<any>(this.employees);
-}
+    //this.dataSource = new MatTableDataSource<any>(this.employees);
+  }
 
   ngOnInit() {
-    return this.employeeService.getEmployees().subscribe(res => this.dataSource.data=res)
+    return this.employeeService.getEmployees().subscribe(res => this.dataSource.data=res);
   }
   ngAfterViewInit() {
     if (this.paginator)
       this.dataSource.paginator=this.paginator;
+    this.dataSource.sort=this.sort;
   }
 
   public getEmployees(): void{
@@ -72,5 +74,10 @@ export class AppComponent implements OnInit{
         tableData: this.dataSource
       }
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
